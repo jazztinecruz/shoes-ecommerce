e2380e02019d2887f88353e83fcf26bcc3c33077
jazztinecruz <me@jazztinecruz.com>
2024-06-19 14:55:43 +0800

    chore: Refactor getItems module to use new Apollo client and queries

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,4 +1,8 @@
+import get from "@core/libraries/get";
+
 const Page: Page = async () => {
+  const { shoes } = await get.items.multiple();
+  console.log(shoes)
   return <div>Page</div>;
 };
 
--- core/libraries/get/items.ts
+++ core/libraries/get/items.ts
@@ -1,6 +1,5 @@
-import { GET_ALL_ITEMS, GET_SINGLE_ITEM } from "../../../hygraph/queries/items";
-import { gql } from "@apollo/client";
 import { getClient } from "@core/clients/apollo";
+import { GET_ALL_SHOES, GET_SINGLE_SHOE } from "@hygraph/queries";
 
 const getItems = {
   multiple: async () => await multiple(),
@@ -12,26 +11,20 @@ export default getItems;
 const multiple = async () => {
   const client = getClient();
   const response = await client.query({
-    query: GET_ALL_ITEMS,
+    query: GET_ALL_SHOES,
   });
 
-  const items = response.data.items;
-  const loading = response.loading;
-  const error = response.error;
-
-  return { items, loading, error };
+  const shoes = response.data.shoes;
+  return { shoes };
 };
 
 const single = async (id: string) => {
   const client = getClient();
   const response = await client.query({
-    query: GET_SINGLE_ITEM,
+    query: GET_SINGLE_SHOE,
     variables: { id },
   });
 
-  const item = response.data.items[0];
-  const loading = response.loading;
-  const error = response.error;
-
-  return { item, loading, error };
+  const shoe = response.data.shoes[0];
+  return { shoe };
 };
